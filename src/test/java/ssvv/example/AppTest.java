70276842a85d8dc5f89a77a30b746f3bf33268ed
ilikekratos <iliealexrusu1@gmail.com>
2023-03-21 17:28:53 +0200

    mesaj

---
+++ src/test/java/ssvv/example/AppTest.java
@@ -0,0 +1,47 @@
+package ssvv.example;
+
+import static org.junit.Assert.assertTrue;
+
+import console.UI;
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    Validator<Student> studentValidator = new StudentValidator();
+    Validator<Tema> temaValidator = new TemaValidator();
+    Validator<Nota> notaValidator = new NotaValidator();
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+    Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testAdd(){
+        service.deleteStudent("100");
+        assertTrue(service.saveStudent("100","alfonso",314)==1);
+    }
+}
