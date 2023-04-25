368f36d45f3b154366d9c13a44a156a898bf29e1
Cristianus <cristian.iorga2000@yahoo.com>
2023-04-25 17:10:29 +0300

    gata fratello

--- src/test/java/ssvv/example/ServiceIncrementalIntegrationTest.java
+++ src/test/java/ssvv/example/ServiceIncrementalIntegrationTest.java
@@ -2,6 +2,7 @@ package ssvv.example;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import ssvv.domain.Pair;
 import ssvv.repository.NotaXMLRepository;
 import ssvv.repository.StudentXMLRepository;
@@ -13,18 +14,18 @@ import ssvv.validation.TemaValidator;
 
 import static org.junit.Assert.assertEquals;
 
-public class BigBang {
+public class ServiceIncrementalIntegrationTest {
     private Service service;
 
     @Before
     public void setUp() {
-        StudentValidator vs = new StudentValidator();
-        TemaValidator ts = new TemaValidator();
-        NotaValidator ns = new NotaValidator();
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        NotaValidator notaValidator = new NotaValidator();
 
-        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(vs, "src/test/java/studenti.xml");
-        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(ns, "src/test/java/note.xml");
-        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(ts, "src/test/java/teme.xml");
+        StudentXMLRepository studentXMLRepository = Mockito.mock(StudentXMLRepository.class);
+        TemaXMLRepository temaXMLRepository = Mockito.mock(TemaXMLRepository.class);
+        NotaXMLRepository notaXMLRepository = Mockito.mock(NotaXMLRepository.class);
 
         service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
     }
@@ -37,35 +38,33 @@ public class BigBang {
     }
 
     @Test
-    public void testAddAssignment() {
-        int result = service.saveTema("1", "Assignment 1", 3, 1);
-        assertEquals(1, result);
-        service.deleteTema("1");
-    }
+    public void testAddStudentAndAssignment() {
+        int addStudentResult = service.saveStudent("1", "John Doe", 933);
+        int addAssignmentResult = service.saveTema("1", "Assignment 1", 3, 1);
 
-    @Test
-    public void testAddGrade() {
-        service.saveStudent("1", "John Doe", 933);
-        service.saveTema("1", "Assignment 1", 3, 1);
-        int result = service.saveNota("1", "1", 9.5, 3, "Good job!");
-        assertEquals(1, result);
+        assertEquals(1, addStudentResult);
+        assertEquals(1, addAssignmentResult);
+
         service.deleteTema("1");
         service.deleteStudent("1");
-        service.deleteNota(new Pair<>("1", "1"));
     }
 
     @Test
-    public void testBigBangIntegration() {
+    public void testAddStudentAssignmentAndGrade() {
         int addStudentResult = service.saveStudent("1", "John Doe", 933);
         int addAssignmentResult = service.saveTema("1", "Assignment 1", 3, 1);
         int addGradeResult = service.saveNota("1", "1", 9.5, 3, "Good job!");
 
+        System.out.println(service.findAllStudents());
+        System.out.println(service.findAllTeme());
+        System.out.println(service.findAllStudents());
+
         assertEquals(1, addStudentResult);
         assertEquals(1, addAssignmentResult);
-        assertEquals(1, addGradeResult);
+        assertEquals(-1, addGradeResult);
 
+        service.deleteNota(new Pair<>("1", "1"));
         service.deleteTema("1");
         service.deleteStudent("1");
-        service.deleteNota(new Pair<>("1", "1"));
     }
 }
