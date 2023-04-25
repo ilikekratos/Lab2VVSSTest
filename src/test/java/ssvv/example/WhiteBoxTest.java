250d10fa71ba37ffa6904b552565cac5e3c6fd77
ilikekratos <iliealexrusu1@gmail.com>
2023-04-25 14:30:16 +0300

    th3

---
+++ src/test/java/ssvv/example/WhiteBoxTest.java
@@ -0,0 +1,51 @@
+package ssvv.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+/**
+ * Unit test for simple App.
+ */
+public class WhiteBoxTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    Validator<Student> studentValidator = new StudentValidator();
+    Validator<Tema> temaValidator = new TemaValidator();
+    Validator<Nota> notaValidator = new NotaValidator();
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+    Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+
+    @Test
+    public void testAddSuccess(){
+        assertEquals(1, service.saveTema("169", "Tema SSVV", 14, 2));
+        service.deleteTema("169");
+    }
+    @Test
+    public void testAddFail(){
+        service.saveTema("169", "Tema SSVV", 14, 2);
+        assertEquals(0, service.saveTema("169", "Tema SSVV", 14, 2));
+        service.deleteTema("169");
+
+    }
+
+}
